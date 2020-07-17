b4a2cd44907cb9318fe4a32cffe69e23a62f6679
wangybest <1319176438@qq.com>
2020-07-17 09:14:29 +0800

    a

--- consumer/src/main/java/com/example/consumer/controller/DemoController.java
+++ consumer/src/main/java/com/example/consumer/controller/DemoController.java
@@ -2,9 +2,7 @@ package com.example.consumer.controller;
 
 import com.example.consumer.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
